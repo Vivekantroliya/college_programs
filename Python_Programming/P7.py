bd36c9b0def231b04cb379811c46cec9070c0d93
Vivekantroliya <145489394+Vivekantroliya@users.noreply.github.com>
2024-02-14 11:21:48 +0530

    Add files via upload

---
+++ Python_Programming/P7.py
@@ -0,0 +1,27 @@
+# Write A Program To Perform Various Operation On List Using Menu.
+
+lst = [2,3,4,5]
+lst1 = [1,11]
+print(lst)
+print(lst1)
+lst.append(6)
+print(lst)
+lst.insert(5,7)
+print(lst)
+lst.remove(7)
+print(lst)
+lst.pop()
+print(lst)
+lst.extend(lst1)
+print(lst)
+lst.reverse()
+print(lst)
+lst.sort()
+print(lst)
+print(len(lst))
+print(min(lst))
+print(max(lst))
+x=slice(6,0,-2)
+print(lst[x])
+
+
