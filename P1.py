64c484bff1eb8b93825fb89eceb6e7c3d5090a0c
Vivekantroliya <145489394+Vivekantroliya@users.noreply.github.com>
2024-02-15 15:51:09 +0530

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,9 @@
+#write a program to input p,r,n and find out interest using simple input output statement.
+
+p = int(input("Enter The Principle Amount : "))
+r = float(input("Enter The Rate Of Interest : "))
+n = float(input("Enter Number Of Years : "))
+
+Interest = p*r*n/100
+
+print("Total Interest Is ",Interest," Rs.")
